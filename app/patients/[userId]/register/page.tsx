55e63d8c8529f0edce66894cc710857e6c52f56d
Mariano Ostric <ostricmariano@gmail.com>
2024-07-12 15:49:38 -0300

    first commit

--- app/patients/[userId]/register/page.tsx
+++ app/patients/[userId]/register/page.tsx
@@ -1,27 +1,31 @@
 import PatientForm from "@/components/forms/PatientForm";
-import { Button } from "@/components/ui/button";
+import RegisterForm from "@/components/forms/RegisterForm";
+import { getUser } from "@/lib/actions/patient.actions";
 import Image from "next/image";
 import Link from "next/link";
+import React from "react";
 
-export default function Home() {
+const Register = async ({ params:{ userId }}: SearchParamProps) => {
+
+  const user = await getUser(userId);
+
   return (
-    <div className="flex h-screen max-h-screen">
-      {/* TODO: OTP Verfication | Passket modal */}
+    <div className="flex h-screen max-h-screen ">
       <section className="remove-scrollbar container my-auto">
-        <div className="sub-container max-w-[496px]">
-          <Image 
+        <div className="sub-container max-w-[860px] flex-1 justify-between">
+          <Image
             src="/assets/icons/logo-full.svg"
             alt="Patient"
             width={1000}
             height={1000}
             className="mb-12 h-10 w-fit"
           />
-          <PatientForm />
+          <RegisterForm user={user} />
 
           <div className="text-14-regular mt-20 flex justify-between">
-           <p className="justify-items-end text-dark-600 xl:text-left">
+            <p className="justify-items-end text-dark-600 xl:text-left">
               ℗ 2024 Care plus
-            </p> 
+            </p>
             <Link href="/?admi=true" className="text-green-500">
               Admin
             </Link>
@@ -29,12 +33,14 @@ export default function Home() {
         </div>
       </section>
       <Image
-        src="/assets/images/onboarding-img.png"
+        src="/assets/images/register-img.png"
         alt="Doctor"
         width={1000}
         height={1000}
-        className="side-img max-w-[50%]"
+        className="side-img max-w-[390px] bg-bottom"
       />
     </div>
   );
-}
+};
+
+export default Register;
---
+++ components/forms/RegisterForm.tsx
@@ -0,0 +1,207 @@
+"use client";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+import { Form, FormControl } from "@/components/ui/form";
+import { UserFormValidation } from "@/lib/validations";
+
+import "react-phone-number-input/style.css";
+import CustomFormField from "../CustomFormField";
+import SubmitButton from "../SubmitButton";
+import { createUser } from "@/lib/actions/patient.actions";
+
+import { FormFieldType } from "./PatientForm";
+import { RadioGroup, RadioGroupItem } from "../ui/radio-group";
+import { FileDiff } from "lucide-react";
+import { Doctors, GenderOptions } from "@/constants";
+import { Label } from "../ui/label";
+import { SelectItem } from "@radix-ui/react-select";
+import Image from "next/image";
+
+const RegisterForm = ({ user }: { user: User }) => {
+  const router = useRouter();
+  const [isLoading, setIsLoading] = useState(false);
+
+  // 1. Define your form.
+  const form = useForm<z.infer<typeof UserFormValidation>>({
+    resolver: zodResolver(UserFormValidation),
+    defaultValues: {
+      name: "",
+      email: "",
+      phone: "",
+    },
+  });
+
+  // 2. Define a submit handler.
+  async function onSubmit({
+    name,
+    email,
+    phone,
+  }: z.infer<typeof UserFormValidation>) {
+    setIsLoading(true);
+    try {
+      const userData = { name, email, phone };
+      const user = await createUser(userData);
+      //console.log(user)
+
+      if (user) router.push(`/patients/${user.$id}/register`);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  return (
+    <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className="space-y-12 flex-1"
+      >
+        <section className="space-y-4">
+          <h1 className="header">Welcome 👋</h1>
+          <p className="text-dark-700">Let us know more about yourself</p>
+        </section>
+
+        <section className="space-y-6">
+          <div className="mb-9 space-y1">
+            <h2 className="sub-header">Personal Information</h2>
+          </div>
+        </section>
+
+        <CustomFormField
+          fieldType={FormFieldType.INPUT}
+          control={form.control}
+          name="name"
+          label="Full name"
+          placeholder="John Doe"
+          iconSrc="/assets/icons/user.svg"
+          iconAlt="User"
+        />
+
+        <div className="flex flex-col gap-6 xl:flex-row">
+          <CustomFormField
+            fieldType={FormFieldType.INPUT}
+            control={form.control}
+            name="email"
+            label="Email"
+            placeholder="Johndoe@gmail.com"
+            iconSrc="/assets/icons/email.svg"
+            iconAlt="User"
+          />
+          <CustomFormField
+            fieldType={FormFieldType.PHONE_INPUT}
+            control={form.control}
+            name="phone"
+            label="Phone Number"
+            placeholder="(555) 123-4567"
+            iconSrc="/assets/icons/email.svg"
+            iconAlt="User"
+          />
+        </div>
+
+        <div className="flex flex-col gap-6 xl:flex-row">
+          <CustomFormField
+            fieldType={FormFieldType.DATE_PICKER}
+            control={form.control}
+            name="birthDate"
+            label="Date of birth"
+          />
+          <CustomFormField
+            fieldType={FormFieldType.SKELETON}
+            control={form.control}
+            name="gender"
+            label="Gender"
+            renderSkeleton={(field) => (
+              <FormControl>
+                <RadioGroup
+                  className="flex gap-6 xl:justify-between"
+                  onValueChange={field.onChange}
+                  defaultValue={field.value}
+                >
+                  {GenderOptions.map((option) => (
+                    <div key={option} className="radio-group">
+                      <RadioGroupItem value={option} id={option} />
+                      <Label htmlFor={option} className="cursor-pointer">
+                        {option}
+                      </Label>
+                    </div>
+                  ))}
+                </RadioGroup>
+              </FormControl>
+            )}
+          />
+        </div>
+
+        <div className="flex flex-col gap-6 xl:flex-row">
+          <CustomFormField
+            fieldType={FormFieldType.INPUT}
+            control={form.control}
+            name="address"
+            label="Address"
+            placeholder="14 street, New Yor"
+          />
+          <CustomFormField
+            fieldType={FormFieldType.INPUT}
+            control={form.control}
+            name="ocupation"
+            label="Opccupation"
+            placeholder="Sofware developer"
+          />
+        </div>
+
+        <div className="flex flex-col gap-6 xl:flex-row">
+          <CustomFormField
+            fieldType={FormFieldType.INPUT}
+            control={form.control}
+            name="emergencyContactName"
+            label="Emergency Contact Name"
+            placeholder="Guardian Name"
+          />
+          <CustomFormField
+            fieldType={FormFieldType.PHONE_INPUT}
+            control={form.control}
+            name="emergencyContactNumber"
+            label="Emergency contact number"
+            placeholder="(555) 123-4567"
+          />
+        </div>
+
+        <section className="space-y-6">
+          <div className="mb-9 space-y1">
+            <h2 className="sub-header">Medical Information</h2>
+          </div>
+        </section>
+        
+        {/* PRIMARY CARE PHYSICIAN */}
+        <CustomFormField
+            fieldType={FormFieldType.SELECT}
+            control={form.control}
+            name="primaryPhysician"
+            label="Primary care physician"
+            placeholder="Select a physician"
+          >
+            {Doctors.map((doctor, i) => (
+              <SelectItem key={doctor.name + i} value={doctor.name}>
+                <div className="flex cursor-pointer items-center gap-2">
+                  <Image
+                    src={doctor.image}
+                    width={32}
+                    height={32}
+                    alt="doctor"
+                    className="rounded-full border border-dark-500"
+                  />
+                  <p>{doctor.name}</p>
+                </div>
+              </SelectItem>
+            ))}
+          </CustomFormField>
+
+        <SubmitButton isLoading={isLoading}> Get Stated </SubmitButton>
+      </form>
+    </Form>
+  );
+};
+
+export default RegisterForm;
