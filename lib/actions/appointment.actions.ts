b79df238a6b28e8bad2cf266fa327ea12f2dae70
Mariano Ostric <ostricmariano@gmail.com>
2024-07-17 09:16:02 -0300

    table finished

--- lib/actions/appointment.actions.ts
+++ lib/actions/appointment.actions.ts
@@ -3,6 +3,8 @@
 import { ID, Query } from "node-appwrite";
 import { APPOINTMENT_COLLECTION_ID, DATABASE_ID, databases } from "../appwrite.config";
 import { parseStringify } from "../utils";
+import { Appointment } from "@/types/appwrite.types";
+import { revalidatePath } from "next/cache";
 
 export const createAppointment = async (appointmnet : CreateAppointmentParams) => {
   try {
@@ -27,6 +29,65 @@ export const getAppointments = async (appointmentId: string) => {
       appointmentId
     );
     return parseStringify(appointment);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export const getRecentAppointments = async () => {
+  try {
+    const appointments = await databases.listDocuments(
+      DATABASE_ID!,
+      APPOINTMENT_COLLECTION_ID!,
+      [Query.orderDesc('$createdAt')]
+    );
+
+    const intialCount = {
+      pendingCount: 0,
+      scheduledCount: 0,
+      cancelledCount: 0
+    }
+    const counts = (appointments.documents as Appointment[]).reduce((acc, appointment) => {
+      if (appointment.status === 'pending') {
+        acc.pendingCount += 1;
+      } else if (appointment.status === 'scheduled') {
+        acc.scheduledCount += 1;
+      } else if (appointment.status === 'cancelled') {
+        acc.cancelledCount += 1;
+      }
+      return acc;
+    }, intialCount);
+
+    const data = {
+      totalCount: appointments.total,
+      ...counts,
+      documents: appointments.documents
+    }
+
+    return parseStringify(data);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export const updateAppointment = async ({ appointmentId, userId, appointment, type }: UpdateAppointmentParams) => {
+  try {
+    const upddatedAppointment = await databases.updateDocument(
+      DATABASE_ID!,
+      APPOINTMENT_COLLECTION_ID!,
+      appointmentId,
+      appointment
+    );
+
+    if(!upddatedAppointment) {
+      throw new Error('Appointment not found');
+    }
+    // TODO SMS notification
+
+    revalidatePath('/admin');
+
+    return parseStringify(upddatedAppointment);
+
   } catch (error) {
     console.log(error);
   }
